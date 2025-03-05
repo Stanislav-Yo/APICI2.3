ba6f582635d2f5e00b1c5ff3a19b4c216c5ff9a8
StasY <yolkhingp@gmail.com>
2025-03-05 21:47:52 +0300

    Some restored

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -20,6 +20,6 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("wrong data"));
+                .body("data", equalTo("some data"));
     }
 }
